72cdeea1262fa1bbebead0587054b23f7a0d7e7d
Gabor Pihaj <gabor.pihaj@gmail.com>
2023-11-18 10:59:07 +0000

    feat!: optionally launch kitty overlay when running file browser (#12)
    As part of this feature the command line arguments have been changed too.
    
    Reviewed-on: https://git.vdx.hu/voidcontext/felis/pulls/12
    Co-authored-by: Gabor Pihaj <gabor.pihaj@gmail.com>
    Co-committed-by: Gabor Pihaj <gabor.pihaj@gmail.com>

--- app/src/bin/felis.rs
+++ app/src/bin/felis.rs
@@ -13,13 +13,29 @@ struct Cli {
 #[derive(Debug, Subcommand)]
 enum Command {
     GetActiveFocusedWindow,
-    OpenInHelix {
-        #[arg(short, long, required_unless_present("file_browser"))]
-        path: Option<PathBuf>,
+    /// Open the given file in helix
+    OpenFile {
+        /// Path to the file to open
+        path: PathBuf,
+        /// Open the file in the helix process running in the given window
         #[arg(short, long)]
         tab_id: Option<u32>,
-        #[arg(short, long, required_unless_present("path"))]
-        file_browser: Option<String>,
+    },
+    /// Run the given file browser / file manager and then open the selected file in helix
+    OpenBrowser {
+        /// Name or path to the executable to run to select the file to open. The given program
+        /// needs to print the path of then file to the standard output, e.g. a propertly configured
+        /// `broot`
+        file_browser: String,
+        /// Open the file in the helix process running in the given window. If not given felis will
+        /// try to determine which helix instance is running in one the parent directories of the
+        /// given file.
+        #[arg(short, long)]
+        tab_id: Option<u32>,
+        /// When true felis will launch a kitty overlay on top the current window, and run the file
+        /// browser there. This is useful when felis is running from an editor.
+        #[arg(short, long, default_value_t = false)]
+        launch_overlay: bool,
     },
 }
 
@@ -32,26 +48,7 @@ async fn main() -> Result<()> {
         Command::GetActiveFocusedWindow => {
             handle_command(&felis::Command::GetActiveFocusedWindow, &kitty).await?
         }
-        Command::OpenInHelix {
-            tab_id,
-            path,
-            file_browser,
-        } => {
-            let path = if let Some(path) = path {
-                path
-            } else if let Some(file_browser) = file_browser {
-                let mut child = tokio::process::Command::new(file_browser)
-                    .stdout(Stdio::piped())
-                    .spawn()?;
-                let mut stdout = child.stdout.take().unwrap();
-                let mut out = String::new();
-                stdout.read_to_string(&mut out).await?;
-                let out = out.trim_end();
-                PathBuf::from(out)
-            } else {
-                panic!("This shouldn't happen")
-            };
-
+        Command::OpenFile { path, tab_id } => {
             handle_command(
                 &felis::Command::OpenInHelix {
                     kitty_tab_id: tab_id,
@@ -60,6 +57,58 @@ async fn main() -> Result<()> {
                 &kitty,
             )
             .await?
+        }
+        Command::OpenBrowser {
+            file_browser,
+            tab_id,
+            launch_overlay,
+        } => {
+            if launch_overlay {
+                let executable = std::env::current_exe()?;
+
+                // TODO: replace this with a KittyCommand
+                let mut cmd = tokio::process::Command::new("kitty");
+                cmd.args([
+                    "@",
+                    "launch",
+                    "--type",
+                    "overlay",
+                    "--cwd",
+                    "current",
+                    executable.as_os_str().to_str().unwrap(),
+                    "open-browser",
+                    file_browser.as_str(),
+                ]);
+
+                if let Some(tab_id) = tab_id {
+                    cmd.arg("--tab-id");
+                    cmd.arg(tab_id.to_string());
+                }
+
+                cmd.spawn()?.wait().await?;
+
+                felis::Response::Ack
+            } else {
+                let mut child = tokio::process::Command::new(file_browser)
+                    .stdout(Stdio::piped())
+                    .spawn()?;
+                let mut stdout = child.stdout.take().unwrap();
+                let mut out = String::new();
+                stdout.read_to_string(&mut out).await?;
+                let out = out.trim_end();
+                let path = PathBuf::from(out);
+
+                handle_command(
+                    &felis::Command::OpenInHelix {
+                        kitty_tab_id: tab_id,
+                        path,
+                    },
+                    &kitty,
+                )
+                .await?;
+
+                felis::Response::Ack
+            }
         }
     };
 
